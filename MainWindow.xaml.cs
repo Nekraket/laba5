ef407a1fcd87c93e7588e529e3ef83962b9bb8bf
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2024-10-10 01:26:54 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MultiEdit
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            textBox.Style = (Style)Resources["ActiveTextStyle"];
+
+            TextBox symmetricalTextBox = FindSymmetricalTextBox(textBox.Name);
+
+            if (symmetricalTextBox != null)
+            {
+                symmetricalTextBox.Style = (Style)Resources["ActiveTextStyle"];
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            textBox.Style = (Style)Resources["TextStyle"];
+
+            TextBox symmetricalTextBox = FindSymmetricalTextBox(textBox.Name);
+
+            if (symmetricalTextBox != null)
+            {
+                symmetricalTextBox.Style = (Style)Resources["TextStyle"];
+            }
+        }
+
+        private TextBox FindSymmetricalTextBox(string name)
+        {
+            switch (name)
+            {
+                case "TextBoxLeft1":
+                    return TextBoxRight1;
+                case "TextBoxLeft2":
+                    return TextBoxRight2;
+                case "TextBoxLeft3":
+                    return TextBoxRight3;
+                case "TextBoxRight1":
+                    return TextBoxLeft1;
+                case "TextBoxRight2":
+                    return TextBoxLeft2;
+                case "TextBoxRight3":
+                    return TextBoxLeft3;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicEditor
+{
+    public partial class MainWindow : Window
+    {
+        private double brushSize = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorBrush_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ColorBrush.SelectedIndex == 0)
+            {
+                DrawingAttributes.Color = Colors.Black;
+            }
+            if (ColorBrush.SelectedIndex == 1)
+            {
+                DrawingAttributes.Color = Colors.Red;
+            }
+            if (ColorBrush.SelectedIndex == 2)
+            {
+                DrawingAttributes.Color = Colors.Green;
+            }
+            if (ColorBrush.SelectedIndex == 3)
+            {
+                DrawingAttributes.Color = Colors.Blue;
+            }
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            brushSize = e.NewValue;
+            if (DrawingAttributes != null)
+            {
+                DrawingAttributes.Height = brushSize;
+                DrawingAttributes.Width = brushSize;
+            }
+        }
+
+        private void SetDrawingMode(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            if (Canvas != null)
+            {
+                if (DrawRadioButton.IsChecked == true)
+                {
+                    Canvas.EditingMode = InkCanvasEditingMode.Ink;
+                }
+                else if (EditRadioButton.IsChecked == true)
+                {
+                    Canvas.EditingMode = InkCanvasEditingMode.Select;
+                }
+                else if (EraseRadioButton.IsChecked == true)
+                {
+                    Canvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                }
+            }
+
+        }
+    }
+}
